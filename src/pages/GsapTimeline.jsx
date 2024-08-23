ed470081aebe1483f0c5edccfd17270507e1bc87
Aymen Shoteri <aymenshoteri@gmail.com>
2024-08-22 20:50:28 -0400

    learning gsap

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,5 +1,41 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+  const timeline1 = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true
+  });
+
+  useGSAP(() => {
+    timeline1.to("#yellow-box", {
+      x: 250,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut"
+    })
+
+    timeline1.to("#yellow-box", {
+      y: -250,
+      scale: 3,
+      rotation: 360, borderRadius: '100%',
+      duration: 2,
+      ease: 'back.inOut'
+
+    })
+
+    timeline1.to("#yellow-box", {
+      x: 500,
+      scale: 1,
+      rotation: 360, borderRadius: '8px',
+      duration: 2,
+      ease: 'back.inOut'
+
+    })
+  }, []);
 
   return (
     <main>
@@ -35,7 +71,13 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if (timeline1.paused()) {
+            timeline1.play();
+          } else {
+            timeline1.pause();
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
